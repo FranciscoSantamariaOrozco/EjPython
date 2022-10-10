3e0a944f4c2a9b4655c245b7a9e0080402fe2ef2
Francisco Orozco <fsantam5bosch@gmail.com>
2022-10-10 17:40:39 +0200

    Dia 4 - Hasta ejercicio 16

--- Day4-Strings/Exercises/exercises15.py
+++ Day4-Strings/Exercises/exercises15.py
@@ -3,3 +3,5 @@ print()
 
 # 15- Cual es el caracter con el index 0 en la string "Coding For All"
 company = "Coding For All"
+Index0 = company[0]
+print("El caracter con el index 0 es: {}".format(Index0))
---
+++ Day4-Strings/Exercises/exercises16.py
@@ -0,0 +1,7 @@
+#Exercises - Day 4
+print()
+
+# 16- Cual es el caracter con el último index en la string "Coding For All"
+company = "Coding For all"
+LastIndex = company[-1]
+print("El último index es: {}".format(LastIndex))
