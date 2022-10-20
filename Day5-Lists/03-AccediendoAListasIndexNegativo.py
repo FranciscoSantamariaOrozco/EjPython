899487bd6d5d4c3b03fdeffd6213649218e504fc
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-10-20 12:54:52 +0100

    Dia 5 - Cuarta parte

--- Day5-Lists/03-AccediendoAListasIndexNegativo.py
+++ Day5-Lists/03-AccediendoAListasIndexNegativo.py
@@ -17,6 +17,36 @@ print(SecondLast)           # mango
 print()
 
 # Desempaquetando items de la lista
-Lst = ['item', 'item2', 'item3', 'item4', 'item5']
+Lst = ['item1', 'item2', 'item3', 'item4', 'item5']
 FirstItem, SecondItem, ThirdItem, *Rest = Lst
 print(FirstItem)
+print()
+
+# Primer Ejemplo
+Frutas = ['banana', 'orange', 'mango', 'lemon', 'lime', 'apple']
+FirstFruit, SecondFruit, ThirdFruit, *Rest = Frutas
+print(FirstFruit)       # banana
+print(SecondFruit)      # orange
+print(ThirdFruit)       # mango
+print(Rest)             # ['lemon', 'lime', 'apple']
+print()
+
+# Segundo ejemplo sobr desempaquetar listas
+First, Second, Third, *Rest, Tenth = [1,2,3,4,5,6,7,8,9,10]
+print(First)         # 1
+print(Second)        # 2
+print(Third)         # 3
+print(Rest)          # [4,5,6,7,8,9]
+print(Tenth)         # 10
+print()
+
+# Tercer ejemplo sobre desempaquetar listas
+Countries = ['Germany', 'France', 'Belgium', 'Sweden', 'Denmark', 'Finland', 'Norway', 'Iceland', 'Estonia']
+gr, fr, bg, sw, *scandic, est = Countries
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(est)
+print()
