9fb8091868f7b119d1ada28a1a5883152ab31538
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-10-31 14:04:46 +0000

    Dia 5 - Hasta ejercicio 13

---
+++ Day5-Lists/Exercises/exercises10.py
@@ -0,0 +1,10 @@
+#Exercises - Day 5
+print()
+
+# 10- Imprime la lista tras modificar una de las compañias.
+
+IT_Companies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+print("Lista sin modificar: ", IT_Companies)
+IT_Companies[0] = "Twitter"
+print("Lista modificada: ", IT_Companies)
+print()
