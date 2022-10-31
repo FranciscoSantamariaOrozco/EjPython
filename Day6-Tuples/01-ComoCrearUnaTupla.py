1ff9316e3437b8bbcacd5be06408331d5cda16de
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-10-31 16:39:12 +0000

    Dia 6 - Comienzo

---
+++ Day6-Tuples/01-ComoCrearUnaTupla.py
@@ -0,0 +1,29 @@
+# Day 6 - Tuplas
+print()
+# Como crear una tupla.
+"""
+Tuplas
+Una tupla es una colección con tipos de dato distintos, e inmutable. Las tuplas son escritas dentro de paréntesis ().
+Cuando una tupla es creada no puede ser modificada. No se pueden añadir, insertar o remover metodos en una tupla ya que no es modificable.
+Sin embargo, existen algunos métodos relaccionados con las tuplas:
+
+tuple(): Para crear una tupla vacia.
+count(): Para contar el número de un ítem especificado en la tupla.
+index(): Para encontrar el index indicado en una tupla.
+operator: Para incluir dos o más tuplas y crear una nueva tupla.
+"""
+
+# Creando una tupla:
+# TuplaVacia = ()
+# TuplaVacia = tuple()
+
+TuplaItems = ("Item1", "Item2", "Item3", "Item4")
+TuplaFrutas = ("Banana", "Orange", "Mango", "Lemon")
+
+
+# Longitud de las tuplas
+
+print("La longitud de la tupla es:", len(TuplaItems))
+print()
+
+
