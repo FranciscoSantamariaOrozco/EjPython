c66928f1b822ce7f5dddd4fba819cce465c4d9a5
Francisco Orozco <fsantam5bosch@gmail.com>
2022-11-01 21:00:44 +0000

    Dia 6 - Tema Terminado

--- Day6-Tuples/03-HaciendoSlicingTuplas.py
+++ Day6-Tuples/03-HaciendoSlicingTuplas.py
@@ -1,3 +1,47 @@
 # Day 6 - Tuplas
 print()
 # Slicing en Tuplas
+# Podemos hacer slice hacia una subtupla especificando un rango de index de inicio y de final en la tupla.
+# Devolverá una nueva tupla con los items especificados.
+
+# Rango de index positivos
+
+Tupla = ('item1', 'item2', 'item3', 'item4', 'item5')
+TodosLosItems = Tupla[0:5]
+print("Todos los items de la tupla:", TodosLosItems)
+SegundoEnAdelante = Tupla[1:]
+print("Todos los items de la tupla desde el segundo:", SegundoEnAdelante)
+MiddleTwo = Tupla[1:3]
+print("los dos items de enmedio", MiddleTwo)
+print()
+
+
+Frutas = ('banana', 'orange', 'mango', 'lemon')
+AllFruits = Frutas[0:4]
+print("Todas las frutas, de la 0 a la 4:", AllFruits)
+AllFruits = Frutas[0:]
+print("Todas las frutas, desde el indice 0: ", AllFruits)
+OrangeMango = Frutas[1:3]
+print("Solo el indice 1 y 2", OrangeMango)
+OrangeToTheRest = Frutas[1:]
+print("Frutas desde el indice uno en adelante:", OrangeToTheRest)
+print()
+
+
+# Rango de index negativos
+
+Tupla = ('item1', 'item2', 'item3', 'item4')
+AllItems = Tupla[-4:]
+print("Todos los items:", AllItems)
+MiddleTwo = Tupla[-3:-1]
+print("Los dos items intermedios:", MiddleTwo)
+print()
+
+Frutas = ('banana', 'orange', 'mango', 'lemon')
+AllFruits = Frutas[-4]
+print("Todas las frutas:", AllFruits)
+OrangeMango = Frutas[-3:-1]
+print("Frutas de la mitad:", OrangeMango)
+OrangeToTheRest = Frutas[-3:]
+print("Desde el -3 hasta el final:", OrangeToTheRest)
+print()
---
+++ Day6-Tuples/04-CambiandoTuplasAListas.py
@@ -0,0 +1,55 @@
+# Day 6 - Tuplas
+print()
+# Cambiando Tuplas a listas.
+# Podemos cambiar una tupla a una lista y una lista a una tupla. La tupla es inmutable, por lo que si
+# quieres modificar una tupla, deberás convertirla primero en una lista.
+
+# Sintaxis
+Tupla = ('item1', 'item2', 'item3', 'item4')
+Lista = list(Tupla)
+
+Fruits = ('banana', 'orange', 'mango', 'lemon')
+print("Tupla de frutas:", Fruits)
+Fruits = list(Fruits)
+print("Lista de frutas", Fruits)
+Fruits[0] = 'apple'
+print("Cambiamos banana por apple:", Fruits)
+Fruits = tuple(Fruits)
+print("Y la devolvemos a una tupla, ya modificada:", Fruits)
+print()
+
+
+# Comprobando items en una tupla.
+# Podemos comprobar si un item existe o no en una tupla usando in, devolviendonos un valor booleano.
+
+# Syntax
+Tupla = ('item1', 'item2', 'item3', 'item4')
+print('item2' in Tupla)
+print()
+
+Fruits = ('banana', 'orange', 'mango', 'lemon')
+print("Se encuentra 'orange' en la tupla frutas? ", 'orange' in Fruits)
+print("Se encuentra 'apple' en la tupla frutas? ", 'apple' in Fruits)
+print()
+
+
+# Juntando tuplas.
+# Podemos juntar dos o más tuplas usando el operador +.
+
+# Syntax
+Tupla1 = ('item1', 'item2', 'item3', 'item4')
+Tupla2 = ('item5', 'item6', 'item7', 'item8')
+Tupla3 = Tupla1 + Tupla2
+print("Tupla 1:", Tupla1)
+print("Tupla2:", Tupla2)
+print("Tupla3", Tupla3)
+print()
+
+
+Fruits = ('banana', 'orange', 'mango', 'lemon')
+Vegetables = ('Tomato', 'Potatoe', 'Cabbage', 'Onion', 'Carrot')
+FruitsAndVegetables = Fruits + Vegetables
+print("Frutas:", Fruits)
+print("Vegetales:", Vegetables)
+print("Frutas y vegetales:", FruitsAndVegetables)
+print()
