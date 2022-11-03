512168e111b26385b9d845548cb5ac00ae91633c
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-11-03 12:58:17 +0000

    Dia 9 - Primera parte

---
+++ Day9-Conditionals/01-Condicionales.py
@@ -0,0 +1,12 @@
+# Day 9 - Condicionales.
+print()
+# Condicionales.
+"""
+    Por defecto, las declaraciones en el script de Python se ejecutan secuencialmente desde arriba hacia abajo.
+    Si la lógica de procesamiento lo requiere, el flujo secuencial de ejecución puede ser alterado de las dos siguientes formas:
+
+    Ejecución condicional: Se ejecutará un bloque de una o más sentencias en caso de que cierta expresión o valor sea verdadera.
+
+    Ejecución repetitiva o en bucle: Un bloque de una o mas sentencias puede ser ejecutado repetitivamente siempre que cierta
+    expresión sea verdadera. Los operadores lógicos y de comparación que aprendimos en las secciones anteriores serán útiles aquí.
+"""
---
+++ Day9-Conditionals/02-IF.py
@@ -0,0 +1,17 @@
+# Day 9 - Condicionales.
+print()
+# Condicional IF
+# Tanto en Python como en otros lenguajes de programación, la palabra clave "if" es utilizada para comprobar
+# si una condición es verdadera antes de ejecutar un bloque de código. Recuerde la sangría despues de los dos puntos.
+
+# Syntax
+"""
+if condición:
+    Esta parte del código funcionará si la condición es cierta.
+"""
+
+A = 3
+if A > 0:
+    print("A es un número positivo.")
+print()
+
