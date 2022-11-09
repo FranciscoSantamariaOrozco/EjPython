d903a452a5c7758510ecb5bd4d115d549d74e7c7
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-11-09 11:22:01 +0000

    Dia 11 - Hasta ejercicio 12

---
+++ Day11-Functions/Exercises/exercises12.py
@@ -0,0 +1,6 @@
+#Exercises - Day 11
+print()
+
+# 12 - Declara una función llamada RemoverItem. Tomará una lista y un item como parámetros. Devuelve la lista con el item removido.
+
+#def RemoverItem():
--- Day11-Functions/Exercises/exercises9.py
+++ Day11-Functions/Exercises/exercises9.py
@@ -4,32 +4,16 @@ print()
 # 9- Declara una función que se llame ListaReversa. Tomará un array como parámetro y devolverá el array a la inversa (usa bucles).
 
 ListaFrutas = ["Manzana", "Pera", "Kiwi", "Platano", "Naranja"]
-SetProgramacion = {"Python", "HTML", "CSS", "JavaScript", "Java"}
-#TuplaItems = ("Item1", "Item2", "Item3", "Item4")
-DiccionarioPersona = {
-    "Nombre":"Francisco",
-    "Apellido":"Orozco",
-    "Edad":"34",
-    "Pais":"Portugal",
-    "Casado":False,
-    "Skills":["Javascript", "React", "Node", "MongoDB", "Python"],
-    "Direccion":{
-        "Calle":"Av. Padre Manuel Nóbrega",
-        "CP":"1000-223"
-    }
-}
+TuplaItems = ("Item1", "Item2", "Item3", "Item4")
 
 def ListaReversa(Array):
-    i = len(Array)
-    while i > 0:
-        print(Array[i])
+    LisInv = []
+    i = len(Array)-1
+    while i > -1:
+        LisInv.append(Array[i])
         i = i -1
-    return None
+    return LisInv
 print(ListaReversa(ListaFrutas))
 print()
-print(ListaReversa(SetProgramacion))
-print()
-#print(ListaReversa(TuplaItems))
-print()
-print(ListaReversa(DiccionarioPersona))
+print(ListaReversa(TuplaItems))
 print()
