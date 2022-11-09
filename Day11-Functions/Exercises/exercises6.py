5b06a653f34cf9696378a0c0b3e44dda0c2899ea
Francisco Orozco <fsantam5bosch@gmail.com>
2022-11-09 21:57:00 +0000

    Merge

--- Day11-Functions/Exercises/exercises6.py
+++ Day11-Functions/Exercises/exercises6.py
@@ -2,8 +2,9 @@
 print()
 
 # 6- Escribe una función que calcule la pendiente de una ecuación lineal.
-del Pendiente(Punto1_X, Punto1_Y,)
-Punto1_X = input("Introduce las X del punto 1: ")
-Punto1_Y = input("Introduce las Y del punto 1: ")
-Punto2_X = input("Introduce las X del punto 2: ")
-Punto2_Y = input("Introduce las Y del punto 2: ")
+
+def Pendiente(Punto1_X, Punto1_Y,):
+    Punto1_X = input("Introduce las X del punto 1: ")
+    Punto1_Y = input("Introduce las Y del punto 1: ")
+    Punto2_X = input("Introduce las X del punto 2: ")
+    Punto2_Y = input("Introduce las Y del punto 2: ")
