6aff1bacb770acb82e36353423e6de31feed363e
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-11-10 15:57:08 +0000

    Dia 13 - Tema terminado

--- Day13-ListComprehension/02-FuncionLambda.py
+++ Day13-ListComprehension/02-FuncionLambda.py
@@ -18,3 +18,40 @@ print()
     x = lambda param1, param2, param3: param1 + param2 + param2
     print(x(arg1, arg2, arg3))
 """
+
+def SumaDosNumeros(a, b):
+    return a + b
+
+print(SumaDosNumeros(3, 5))
+print()
+
+# ahora vamos a cambiar la función por una función lambda:
+
+SumaDosNumeros = lambda a, b: a + b
+print(SumaDosNumeros(3, 5))
+print()
+
+# Función lambda autoinvocada
+(lambda a, b: a + b)(3, 5)
+print((lambda a, b: a + b)(3, 5))
+
+Cuadrado = lambda a: a * a
+print(Cuadrado(5))
+Cubo = lambda a: a * a * a
+print(Cubo(3))
+
+# Con múltiples variables
+
+MultipleVariables = lambda a, b, c: a ** 2 - 3 * b + 4 * c
+print(MultipleVariables(5, 5, 3))
+print()
+
+# Función lambda dentro de otras funciones
+
+def Potencia(x):
+    return lambda n : x ** n
+Cubo = Potencia(2)(3)
+print(Cubo)
+DosElevadoCinco = Potencia(2)(5)
+print(DosElevadoCinco)
+print()
