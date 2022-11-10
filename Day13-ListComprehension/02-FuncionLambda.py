695d3e0b4288119529edea4d8ee6cc56fd3e452a
NekkoCat-Thinkpad <fsantam5bosch@gmail.com>
2022-11-10 12:37:51 +0000

    Dia 13 - Segunda parte

---
+++ Day13-ListComprehension/02-FuncionLambda.py
@@ -0,0 +1,20 @@
+# Day 13 - List comprehension.
+print()
+# Función Lambda
+"""
+    La función lambda es una pequeña función anónima sin nombre. Puede tomar cualquier numero de argumentos, pero sólo una expresión.
+    La función lambda es similar a la función "anonymous" en JavaScript. 
+    La necesitamos cuando queremos crear una función anónima dentro de otra función.
+"""
+
+# Creando una función lambda
+"""
+    para crear una función lambda podemos usar la palabra clave "lambda" seguida de los parámetros, seguida de la expresión.~
+    La función lambda no utiliza return ya que ya devuelve explícitamente el return.
+"""
+
+# Syntax
+"""
+    x = lambda param1, param2, param3: param1 + param2 + param2
+    print(x(arg1, arg2, arg3))
+"""
